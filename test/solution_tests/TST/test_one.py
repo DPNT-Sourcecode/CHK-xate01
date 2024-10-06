2349cff8250bd0a6145bf4d555d8ed62dcc94692
sbx_user1051 <sbx_user1051@169.254.117.109>
2024-10-06 17:44:46 +0000

    Sun Oct 06 17:44:46 UTC 2024

--- test/solution_tests/TST/test_one.py
+++ test/solution_tests/TST/test_one.py
@@ -8,6 +8,9 @@ class TestSum():
     def test_eight_A(self):
         assert checkout_solution.checkout("AAAAAAAA") == 360
 
+    def test_one_A(self):
+        assert checkout_solution.checkout("A") == 50
+
     def test_two_A(self):
         assert checkout_solution.checkout("AA") == 100
 
@@ -26,6 +29,8 @@ class TestSum():
     def test_not_recogised_sku(self):
         assert checkout_solution.checkout("E") == -1
         
+    def test_no_sku(self):
+        assert checkout_solution.checkout("") == -1
 
 
 
