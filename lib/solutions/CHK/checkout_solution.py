1b76bc1b64bbfe8ba7f5f7f8f36cd15d60820695
sbx_user1051 <sbx_user1051@169.254.127.125>
2024-10-06 17:13:02 +0000

    Sun Oct 06 17:13:02 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,10 +3,7 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    sku_count = {}
     for char in skus:
         if char not in ['ABCD']:
             return -1
-        
-
-
-
